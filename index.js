ec463445b9df231909b9f772ec122c12220e3efd
Your Name <you@example.com>
2022-11-24 15:03:29 +0100

    set up server

---
+++ index.js
@@ -0,0 +1,113 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+// Database Connection
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0avqkuj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
+async function run() {
+  try {
+    const usersCollection = client.db('buyselldb').collection('users');
+    //  Save user email & generate JWT
+    //  app.put('/user/:email', async (req, res) => {
+    //   const email = req.params.email
+    //   const user = req.body
+    //   const filter = { email: email }
+    //   const options = { upsert: true }
+    //   const updateDoc = {
+    //     $set: user,
+    //   }
+    //   const result = await usersCollection.updateOne(filter, updateDoc, options)
+    //   console.log(result)
+
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //     expiresIn: '1d',
+    //   })
+    //   console.log(token)
+    //   res.send({ result, token })
+    // })
+
+    
+    app.get('/jwt', async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        if (user) {
+            const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            return res.send({ accessToken: token });
+        }
+        res.status(403).send({ accessToken: '' })
+    });
+
+
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        console.log(user);
+        // TODO: make sure you do not enter duplicate user email
+        // only insert users if the user doesn't exist in the database
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+    });
+
+    
+    // Get Bookings
+    // app.post('/bookings',async (req, res) => {
+    //   const booking = req.body
+    //   console.log(booking)
+    //   const result = await bookingsCollection.insertOne(booking)
+
+      // console.log('result----->', result)
+      // sendMail(
+      //   {
+      //     subject: 'Booking Successful!',
+      //     message: `Booking Id: ${result?.insertedId}, TransactionId: ${booking.transactionId}`,
+      //   },
+      //   booking?.guestEmail
+      // )
+    //   res.send(result)
+    // })
+    console.log('Database Connected...')
+  } finally {
+  }
+}
+
+run().catch(err => console.error(err))
+
+app.get('/', (req, res) => {
+  res.send('Server is running... in session')
+})
+
+app.listen(port, () => {
+  console.log(` buy and sell Server is running...on ${port}`)
+})
